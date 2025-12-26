1ee818df91ca275c2898a516d72476ef6cc7ad12
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-26 19:38:28 +0300

    53. Ingestion implementation

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,23 @@
 import os
 from dotenv import load_dotenv
-
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
-    print(os.environ['PINECONE_API_KEY'])
+    loader = TextLoader("C:\\Users\\diomi\\PycharmProjects\\rag-gist\\mediumblog1.txt")
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(api_key = os.environ.get("OPENAI_API_KEY"))
+    print("ingesting...")
+    PineconeVectorStore.from_documents(texts,embeddings,index_name=os.environ['INDEX_NAME'])
+    print("finish")
